cb55f04db5ff3f17b88f72f7c1d9a8676ccecf0e
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-27 11:14:47 +0530

    Create socket.js

---
+++ src/socket.js
@@ -0,0 +1,10 @@
+// src/socket.js
+import { io } from "socket.io-client";
+
+export const createSocket = (token) => {
+    return io("http://localhost:5000", {
+        auth: {
+            token
+        }
+    });
+};
