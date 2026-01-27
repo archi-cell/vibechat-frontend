af55b7c9b2ca8f5d0055010ade21319a265f23ec
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-27 11:25:57 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 export const createSocket = (token) => {
-    return io("https://vibechat-backend-vf0o.onrender.com", {
+    return io(import.meta.env.VITE_BACKEND_URL,{
         auth: {
             token
         }
