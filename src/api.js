b61f9d806411677f6cea1e1efad3cf32182614a6
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-27 11:19:01 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://vibechat-backend-vf0o.onrender.com",
 });
 
 export default API;
