4340cb0d3ed007a82a7b8b1ccf9b9d0a0d534875
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-27 11:30:39 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,7 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
 export const createSocket = (token) => {
-    return io(import.meta.env.VITE_BACKEND_URL,{
-        auth: {
-            token
-        }
-    });
+  return io(import.meta.env.VITE_BACKEND_URL, {
+    auth: { token },
+  });
 };
